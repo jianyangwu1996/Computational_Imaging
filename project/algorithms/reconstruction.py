5f5622455d6d9cd8bd9f2fbc66b769df10116637
jianyangwu1996 <104099688+jianyangwu1996@users.noreply.github.com>
2023-02-07 23:58:27 +0100

    unkown position ePIE temp

--- project/algorithms/reconstruction.py
+++ project/algorithms/reconstruction.py
@@ -110,49 +110,57 @@ def epie(ptychogram: np.ndarray, positions, shape_obj, n_iter=100, a=1, b=1, gue
         return recon_obj, guess_probe
 
 
-# def epie_unknown_pos(patterns, guess_obj, guess_probe, guess_positions, n_iter=150, delta=2, nu=30, beta=8e3, tau=0.1):
-#     """
-#     recovery procedure of the unknown-position ePIE
-#     :param patterns: the recorded diffraction patterns
-#     :param guess_obj: initial randomly guessed object
-#     :param guess_probe: initial randomly guessed probe
-#     :param guess_positions: all-zero positions
-#     :param n_iter: the number of iteration
-#     :param delta: the stagnation sign of position refinement process
-#     :param nu: the least number of iterations being circled before the next round of the process of identifying
-#                 and repositioning the IPs
-#     :param beta:
-#     :param tau:
-#     :return: retrieved complex object, probe and scan position
-#     """
-#
-#     n_IPs = 1
-#     NRMSEs = [0] * len(guess_positions)
-#     e = [(0,0)] * len(guess_positions)
-#     for n in range(n_iter):
-#         for position, pattern, i in zip(guess_positions, patterns, range(len(guess_positions))):
-#             phi = guess_obj * guess_probe
-#             Phi = ft(phi)
-#             phase_Phi = np.exp(1j * np.angle(Phi))
-#             Phi_corrected = np.sqrt(pattern) * phase_Phi
-#             NRMSEs[i] = nrmse(pattern, Phi_corrected)
-#             temp_obj = guess_obj.copy()
-#             guess_obj = update_obj(guess_obj, guess_probe, position)
-#             guess_probe = update_probe(guess_probe, temp_obj, position)
-#             e = cross_correlation(guess_obj, temp_obj)
-#             position += beta * e[i]
-#         maxshift =
-#         beta = update_beata()
-#
-#         if n+1>2 & maxshift<delta & n+1>n_IPs+nu:
-#             n_IPs =n
-#             IPs_index = np.where(NRMSEs > NRMSEs.min()+tau)
-#             IPs = guess_positions[IPs_index]
-#             CPs = list(set(guess_positions) - set(IPs))
-#             for i in IPs_index:
-#                 IP = guess_positions[i]
-#                 CCs = []
-#                 CC = corr()
-#                 index = np.argmax(CCs)
-#                 guess_positions[i] = CPs[index]
-#             beta = np.square()
+def epie_unknown_pos(patterns, guess_obj, guess_probe, guess_positions, n_iter=150, delta=2, nu=30,
+                     alpha = 5e-3, beta=8e3, tau=0.1):
+    """
+    recovery procedure of the unknown-position ePIE
+    :param patterns: the recorded diffraction patterns
+    :param guess_obj: initial randomly guessed object
+    :param guess_probe: initial randomly guessed probe
+    :param guess_positions: all-zero positions
+    :param n_iter: the number of iteration
+    :param delta: the stagnation sign of position refinement process
+    :param nu: the least number of iterations being circled before the next round of the process of identifying
+                and repositioning the IPs
+    :param beta:
+    :param tau:
+    :return: retrieved complex object, probe and scan position
+    """
+
+    n_IPs = 1
+    e = [(0, 0)] * len(guess_positions)
+    beta = list[beta] * len(guess_positions)
+    loss = []
+    for n in range(n_iter):
+        NRMSEs = []
+        for position, pattern, i in zip(guess_positions, patterns, range(len(guess_positions))):
+            phi = guess_obj * guess_probe
+            Phi = ft(phi)
+            phase_Phi = np.exp(1j * np.angle(Phi))
+            Phi_corrected = np.sqrt(pattern) * phase_Phi
+            NRMSE = nrmse(pattern, Phi)
+            temp_obj = guess_obj.copy()
+            guess_obj = update_obj(guess_obj, guess_probe, position)
+            guess_probe = update_probe(guess_probe, temp_obj, position)
+            e = cross_correlation(guess_obj, temp_obj)
+            position += beta[i] * e[i]
+            NRMSEs.append(NRMSE)
+        loss.append(NRMSEs)
+        maxshift = np.abs(beta * e).max()
+        k =                     # cross-correlation coefficient between two sets of successive retrieved position errors
+        beta = update_beata()   # k>0.3 beta * 1.1, k<-0.3 beta * 0.9
+
+        if n+1>2 & maxshift<delta & n+1>n_IPs+nu:
+            n_IPs =n
+            IPs_index = np.where(NRMSEs > NRMSEs.min()+tau)
+            IPs = guess_positions[IPs_index]
+            CPs = list(set(guess_positions) - set(IPs))
+            for i in IPs_index:
+                IP = guess_positions[i]
+                CCs = []
+                CC = corr()
+                index = np.argmax(CCs)
+                guess_positions[i] = CPs[index]
+            beta = np.square(loss[n] / np.min(loss[n])+alpha)
+
+    return guess_obj, guess_probe, guess_positions, loss
